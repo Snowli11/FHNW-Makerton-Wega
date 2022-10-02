fa5bbe4aad537e10135d39b531e1b9aeeab52c45
Snowli11 <62450413+Snowli11@users.noreply.github.com>
2022-10-02 09:27:16 +0200

    v1.0.0

--- source/test.py
+++ source/test.py
@@ -1,25 +1,49 @@
-from scipy.fft import fft, fftfreq, rfft, rfftfreq
+from scipy.fft import fft, fftfreq, irfft, rfft, rfftfreq
 import numpy as np
 import pandas as pd
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
+import csv
 
-import plotly.plotly as py
-import plotly.graph_objs as go
-import plotly.figure_factory as ff
-
+f = r'C:\Users\pjeiz\Project Makertron FHNW\FHNW-Makerton-Wega\XY_data\wrist_XY_01_filtered.csv'
 raw_df = pd.read_csv(r'C:\Users\pjeiz\Project Makertron FHNW\FHNW-Makerton-Wega\XY_data\wrist_XY_01.csv')
-rslt_df = raw_df[raw_df['label'] == 14]
-print(rslt_df)
-df = pd.DataFrame(rslt_df, columns= ['Accelerometer X']).to_numpy()
+print(raw_df)
+df = pd.DataFrame(raw_df, columns= ['Accelerometer Z']).to_numpy()
 print(df)
 
 yf = rfft(df)
+#yf = yf.astype(int)
 print(yf)
 # Number of sample points
 N = len(df)
 # sample spacing
-T = 1.0 / 256
+fs = 256
+T = 1.0 / fs
 xf = rfftfreq(N, T)
-print(xf)
+xf_int = xf.astype(int)
+print(len(xf))
 
-fig = go.Figure([go.Scatter(x=xf['Frequency'], y=yf['Amplitude'])])
-fig.show()
+xf_filtered = []
+
+for index, x in enumerate(xf):
+    if x >= 15:
+        yf[index] = 0
+    
+
+
+plt.figure(figsize=(20,10))
+
+plt.plot(xf[:int(len(xf)*0.2)], np.abs(yf)[:int(len(xf)*0.2)])
+
+plt.show()
+
+
+
+print(len(yf))
+
+
+print(yf)
+
+test = pd.DataFrame(irfft(yf, 1))
+
+test.to_csv(f)
